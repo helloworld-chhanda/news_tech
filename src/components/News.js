9dbfcd9730ac0e820fe95c2a42b4ffad91d9105a
Chhanda Naskar <chhandanaskar.official@gmail.com>
2022-06-20 16:30:12 +0530

    first

--- src/components/News.js
+++ src/components/News.js
@@ -1,19 +1,26 @@
 import './News.css';
 import React,{  useEffect, useState } from 'react';
 import axios from 'axios';
+import logo from '../images/logo.png';
 
 function News() {
+  const [isLoading,setIsLoading] = useState(true);
   const [allNews,setAllNews] = useState([]);
   const getNews = async (search='apple') =>{
-    if(!search) return;
+    setIsLoading(true);
+    if(!search) {
+      setIsLoading(false);
+      return;
+    }
     const response = await axios.get(`https://newsapi.org/v2/everything?q=${search}&from=2022-06-16&to=2022-06-16&sortBy=popularity&apiKey=67a73fe9e2034c589ed00342ef80e50d`)
     const articles = response.data.articles;
     setAllNews(articles);
+    setIsLoading(false);
     console.log(articles);
   }
   const [quote,setQuote] = useState('');
   const getQuote = async () =>{
-    let response = await axios.get(`https://api.quotable.io/random`)
+    let response = await axios.get(`https://api.quotable.io/random`);
     let content = response.data.content;
     console.log(response);
     setQuote(content);
@@ -31,8 +38,7 @@ function News() {
   return (
     <div className='container'>
       <div className='logo-container'>
-        <div className='logo'>
-        </div>
+        <img className='logo' alt='logo' src={logo}/>
       </div>
       <div className='quote'>{quote}</div>
       <div className='search-parent'>
@@ -42,14 +48,16 @@ function News() {
       </div>
       <div className='image-organize'>
       {
-        allNews.map((news,index)=>{
+        isLoading ? <div className='loader'></div> : allNews.map((news,index)=>{
           return (
             <div className='news-container' key={index}>
               <div className='news-image-container'>
                 <img className='news-image' alt="newsImage" src={news.urlToImage}/>
-                <div className='author'>
-                  {news.author}
-                </div>
+                {news.author ? (
+                  <div className='author'>
+                    {news.author}
+                  </div>
+                ):null}
               </div>
               <div className='news-content'>
                   {news.title}
